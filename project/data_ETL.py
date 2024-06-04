96130468135579524889fc2692dbd48b23164759
nathan-vo810 <anh.vnn810@gmail.com>
2024-06-04 10:48:35 +0200

    Update Data Pipeline

--- project/data_ETL.py
+++ project/data_ETL.py
@@ -3,7 +3,6 @@ import zipfile
 import os
 import sqlite3
 import logging
-import json
 import pandas as pd
 
 # Configure logging
@@ -12,30 +11,24 @@ logger = logging.getLogger(__name__)
 
 config = {
     "data_dir": "./data",
+    "database": "MADE_Project.sqlite",
     "datasets": [
     {
         "url": "https://api.worldbank.org/v2/en/indicator/EN.ATM.CO2E.KT?downloadformat=csv",
         "filename": "API_EN.ATM.CO2E.KT_DS2_en_csv_v2",
         "skiprows": 4,
-        "db_name": "CO2_Emission.sqlite",
+        "skipcols": ["Country Code", "Indicator Name", "Indicator Code"],
         "table_name": "CO2_Emission"
     },
     {
         "url": "https://api.worldbank.org/v2/en/indicator/EG.FEC.RNEW.ZS?downloadformat=csv",
         "filename": "API_EG.FEC.RNEW.ZS_DS2_en_csv_v2",
         "skiprows": 4,
-        "db_name": "Renewable_Energy_Consumption.sqlite",
+        "skipcols": ["Country Code", "Indicator Name", "Indicator Code"],
         "table_name": "Renewable_Energy_Consumption"
-    },
-    {
-        "url": "https://api.worldbank.org/v2/en/indicator/EG.ELC.RNEW.ZS?downloadformat=csv",
-        "filename": "API_EG.ELC.RNEW.ZS_DS2_en_csv_v2",
-        "skiprows": 4,
-        "db_name": "Renewable_Electricity_Output.sqlite",
-        "table_name": "Renewable_Electricity_Output"
     }],
     "start_year": 1990,
-    "end_year": 2015
+    "end_year": 2020
 }
 
 def download_and_extract(url, extract_to):
@@ -70,15 +63,18 @@ def read_csv_file(data_dir, filename, skiprows):
         raise
 
 
-def transform_data(df):
+def transform_data(df, skipcols):
     try:
+        # Drop unnecessary columns
+        df.drop(skipcols, axis='columns', inplace=True)
+
         # Remove column with all NaN
         df.dropna(axis=1, how='all', inplace=True)
 
         # Get year range of the current dataset
         data_year_range = df.columns[4:].to_list()
 
-        # Set year range the same for all dataset (1990 - 2015)
+        # Set year range the same for all dataset (1990 - 2020)
         year_range = [str(year) for year in range(config['start_year'], config['end_year']+1)]
         excluded_year = list(set(data_year_range) - set(year_range))
         df.drop(excluded_year, axis = 1, inplace=True)
@@ -87,7 +83,7 @@ def transform_data(df):
         df.dropna(subset=year_range, how='all', inplace=True)
 
         # Fill NaN values with 0
-        df.fillna(0, inplace=True)
+        df.bfill(axis=0, inplace=True)
 
         logger.info("Data transformation complete")
     except Exception as e:
@@ -109,10 +105,13 @@ def save_to_sqlite(df, data_dir, db_name, table_name):
 def main():
     datasets = config['datasets']
     data_dir = config['data_dir']
+    database = config['database']
 
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
     logger.info("Data directory is set up at: %s", data_dir)
+
+    dfs = []
 
     for i, dataset in enumerate(datasets):
         logger.info("-" * 30)
@@ -121,10 +120,16 @@ def main():
         try:
             download_and_extract(dataset['url'], data_dir)
             df = read_csv_file(data_dir, dataset['filename'], dataset['skiprows'])
-            transform_data(df)
-            save_to_sqlite(df, data_dir, dataset['db_name'], dataset['table_name'])
+            transform_data(df, dataset['skipcols'])
+            dfs.append(df)
         except Exception as e:
             logger.error("Error processing dataset: %s", e)
+
+    countries = set.intersection(*[set(df['Country Name'].values) for df in dfs])
+
+    for i, df in enumerate(dfs):
+        df = df[df['Country Name'].isin(countries)]
+        save_to_sqlite(df, data_dir, database, datasets[i]['table_name'])
 
 
 if __name__ == '__main__':
